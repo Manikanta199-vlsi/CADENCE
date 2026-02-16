1befac6692a79b2e92f8719571475b89a13572b1
Manikanta <manitrade789@gmail.com>
2026-02-16 19:41:04 +0530

    Update mmmc.tcl

--- filess/mmmc.tcl
+++ filess/mmmc.tcl
@@ -1,30 +1,62 @@
-# Library set
+# -------------------------------------------------
+# Library Set
+# -------------------------------------------------
+
 create_library_set \
   -name lib_slow \
   -timing {/home/install/FOUNDRY/digital/90nm/dig/lib/slow.lib}
 
-# RC corner
+
+# -------------------------------------------------
+# Timing Condition
+# -------------------------------------------------
+
+create_timing_condition \
+  -name tc_slow \
+  -library_sets {lib_slow}
+
+
+# -------------------------------------------------
+# RC Corner
+# -------------------------------------------------
+
 create_rc_corner \
   -name rc_typ
 
-# Delay corner
+
+# -------------------------------------------------
+# Delay Corner
+# -------------------------------------------------
+
 create_delay_corner \
   -name dc_slow \
-  -library_set lib_slow \
+  -timing_condition tc_slow \
   -rc_corner rc_typ
 
-# Constraint mode
+
+# -------------------------------------------------
+# Constraint Mode
+# -------------------------------------------------
+
 create_constraint_mode \
   -name func \
   -sdc_files {vm.sdc}
 
-# Analysis view
+
+# -------------------------------------------------
+# Analysis View
+# -------------------------------------------------
+
 create_analysis_view \
   -name view_slow \
   -constraint_mode func \
   -delay_corner dc_slow
 
-# Apply
+
+# -------------------------------------------------
+# Apply Views
+# -------------------------------------------------
+
 set_analysis_view \
   -setup view_slow \
   -hold view_slow
